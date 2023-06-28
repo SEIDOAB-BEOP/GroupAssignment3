6848f3060786d2ae8ad796293394fc98d3515f27
Martin Lenart <martin_lenart@icloud.com>
2023-06-28 15:16:31 +0200

    Add project files.

---
+++ GroupAssignment3/StudentList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace GroupAssignment3
+{
+    /*
+        class StudentList: IStudentList
+        {
+            const int MaxNrOfStudents = 50;
+            string[] students;
+
+            private int _NrOfStudents = 0;
+            public int NrOfStudents => _NrOfStudents;
+
+            public override string ToString()
+            {
+                string sRet = "";
+                for (int i = 0; i < _NrOfStudents; i++)
+                {
+                    sRet += $"{students[i],-15}";
+                    if ((i + 1) % 5 == 0)
+                        sRet += "\n";
+                }
+                return sRet;
+            }
+
+            public void Seed()
+            {
+                students = new string[MaxNrOfStudents];
+                students[0] = "Harry";
+                students[1] = "Hermione";
+                students[2] = "Albus";
+                students[3] = "Severus";
+                students[4] = "Ron";
+                students[5] = "Draco";
+                students[6] = "Frodo";
+                students[7] = "Gandalf";
+                students[8] = "Sam";
+                students[9] = "Peregrin";
+                students[10] = "Saruman";
+                students[11] = "Spock";
+                students[12] = "Khan";
+                students[13] = "Scotty";
+                students[14] = "Uhura";
+                students[15] = "Nero";
+                students[16] = "Sulu";
+                students[17] = "Woody";
+                students[18] = "Buzz";
+                students[19] = "Nemo";
+                students[20] = "Doris";
+                students[21] = "Edna";
+                students[22] = "Mike";
+                students[23] = "Sully";
+                students[24] = "Wall-E";
+                students[25] = "Coco";
+                students[26] = "Jack";
+                students[27] = "Elizabeth";
+                students[28] = "Barbosa";
+                students[29] = "Angelica";
+                students[30] = "Pintel";
+                students[31] = "Carina";
+
+                _NrOfStudents = 30;
+            }
+        }
+    */
+}
